042cb2038f62cf1c3959f43142039b22affdd425
rrs108 <shallwenjoy@gmail.com>
2024-11-10 01:56:56 +0900

    create prj

---
+++ android/app/src/main/kotlin/com/example/hr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  var a = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            print(a);
+            a++;
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Header'),
+        ),
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, idx) {
+            return ListTile(
+            leading: Image.asset('spaceEye.jpg'),
+            title: Text('data'),
+            );
+          },
+        ),
+      )
+    );
+  }
+}
