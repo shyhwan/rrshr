98dd1f0ed98f3cae7f2958b531aa3b511d1f3d67
rrs108 <shallwenjoy@gmail.com>
2024-11-10 02:15:07 +0900

    두번째

--- lib/testBranch01.dart
+++ lib/testBranch01.dart
@@ -1,4 +1,15 @@
 
-void main() {
+import 'package:flutter/cupertino.dart';
 
+void main() {
+  runApp(Testapp());
+}
+
+class Testapp extends StatelessWidget {
+  Testapp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
